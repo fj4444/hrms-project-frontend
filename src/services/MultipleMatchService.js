47436ab9bb80bda640d1d2d2dd0673feef414f4f
fj4444 <2020011850@secoder.net>
2022-12-13 15:20:18 +0800

    multiplematchcard

--- src/services/MultipleMatchService.js
+++ src/services/MultipleMatchService.js
@@ -1,30 +1,11 @@
 export default class MultipleMatchService {
     controllerUrl = `${process.env.REACT_APP_API_URL}/jobadverts`;
-  
-    getAllByIsActiveForList() {
-      // return axios.get(`${this.controllerUrl}/forlist/byisactive`, {
-      //   params: {
-      //     isActive,
-      //     page,
-      //     size,
-      //     sortDirection,
-      //     sortProperties,
-      //   },
-      // });
-      const result = {
-        data: [
-          {
-            id: 1,
-            betDeadline: "2022-11-01",
-            startAt: "2022-11-01",
-            host: "英格兰",
-            guest: "法国",
-            hostRate: 10,
-            guestRate: 20
-          }
-        ],
-        length: 1,
-      };
-      return result;
+
+    getsize() {//获取奖池大小，champion代表半决赛和决赛奖池
+        const result = {
+            champion: 15,
+            best: 10,
+        };
+        return result;
     }
-  }
+}
